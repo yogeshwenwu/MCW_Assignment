05f553e809f38503026537e4d55cd68561ee7a2b
Yogesh M V <134631738+yogeshwenwu@users.noreply.github.com>
2025-02-07 15:19:25 +0530

    Update crtfenv.sh

--- crtfenv.sh
+++ crtfenv.sh
@@ -1,1 +1,12 @@
-ls
+#!/bin/bash
+
+source /home/groot/anaconda3/etc/profile.d/conda.sh
+
+conda create --name env_py_3.10_tf python=3.10
+echo "<-----CreatedEnv----->"
+conda activate env_py_3.10_tf
+echo "Current conda environment: $CONDA_DEFAULT_ENV"
+pip install tensorflow
+echo "<-----Installed Successfully----->"
+conda deactivate
+echo "Back to base"

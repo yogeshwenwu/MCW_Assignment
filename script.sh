9eff8c26d167f7156103da7698cf1cd3b3568ca0
Yogesh-M-V <yogeshmv744@gmail.com>
2025-04-22 11:04:40 +0530

    Created script file to automate this process

--- script.sh
+++ script.sh
@@ -2,18 +2,19 @@
 
 source /mnt/mydisk/yogesh/anaconda3/etc/profile.d/conda.sh
 
-conda create --name env_py_3.10_torch_2.4 python=3.10
 echo "<--------Activated-------->"
 conda activate env_py_3.10_torch_2.4
 echo "Current conda environment: $CONDA_DEFAULT_ENV"
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cpu
+python ./Img_Clf_t2.4/model.py
 conda deactivate
 echo "<--------Deactivated-------->"
 
-conda create --name env_py_3.11_torch_2.5 python=3.11
-conda activate env_py_3.11_torch_2.5
+sleep 2 
+
+
+conda activate env_py_3.11_tf
 echo "<--------Activated-------->"
 echo "Current conda environment: $CONDA_DEFAULT_ENV"
-pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu
-echo "<--------Deactived----->"
+python ./Img_Clf_tf/model.py
+echo "<--------Deactivated----->"
 conda deactivate

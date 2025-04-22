c732e811454d1a30f61c3b372acb68ef262f1e37
Yogesh-M-V <yogeshmv744@gmail.com>
2025-04-22 11:03:27 +0530

    Updated the env creation

--- crtfenv.sh
+++ crtfenv.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-source /home/groot/anaconda3/etc/profile.d/conda.sh
+source /mnt/mydisk/yogesh/anaconda3/etc/profile.d/conda.sh
 
-conda create --name env_py_3.10_tf python=3.10
-echo "<-----CreatedEnv----->"
-conda activate env_py_3.10_tf
+conda create --name env_py_3.10_torch_2.4 python=3.10
+conda activate env_py_3.10_torch_2.4
+echo "<--------Activated-------->"
+echo "Current conda environment: $CONDA_DEFAULT_ENV"
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cpu
+conda deactivate
+echo "<--------Deactivated-------->"
+
+conda create --name env_py_3.11_tf python=3.11
+conda activate env_py_3.11_tf
+echo "<--------Activated-------->"
 echo "Current conda environment: $CONDA_DEFAULT_ENV"
 pip install tensorflow
 echo "<-----Installed Successfully----->"
 conda deactivate
-echo "Back to base"
+echo "<--------Deactivated-------->"
